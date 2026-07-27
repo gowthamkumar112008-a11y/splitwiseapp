2056d0f470b9453bcccb4db0867a2aa21d2dd16c
gowtham <gowthamkumar112008@gmail.com>
2026-07-27 10:30:37 +0530

    1.3

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ package splitwiseapp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("===Splitwise Console===");
+        String payerName = "Gowtham";
         double totalAmount=500.0;
         int numFriends=3;
         System.out.println("Track shared expenses with friends.");
         System.out.println();
         System.out.println("Ready. More features coming in the next lessons.");
+        System.out.println(payerName);
         System.out.println(totalAmount);
         System.out.println(numFriends);
     }
