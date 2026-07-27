9cc9a7c1983751ae6204a2e132a8ce9fd10c5b15
gowtham <gowthamkumar112008@gmail.com>
2026-07-27 13:32:28 +0530

    1.8

--- Main.java
+++ Main.java
@@ -1,34 +1,60 @@
 package splitwiseapp;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("===Splitwise Console===");
         Scanner input=new Scanner(System.in);
+        ArrayList<String> friends = new ArrayList<>();
         boolean start=true;
         while(start) {
             System.out.println("    ==menu==");
             System.out.println("1.Record Expense");
-            System.out.println("2.Quit");
+            System.out.println("2.Add friends");
+            System.out.println("3.List friends");
+            System.out.println("0.Quit");
             System.out.print("Enter choice : ");
             int choice = input.nextInt();
             input.nextLine();
 
             switch (choice) {
                 case 1 -> {
-                    System.out.print("Who paid? ");
-
-                    String payerName = input.nextLine();
-                    System.out.print("Total amount Rs ? ");
-                    double totalAmount = input.nextDouble();
-                    int numFriends = 3;
-                    double perPersonShare = totalAmount / numFriends;
-                    String expenseLine = "%s paid ₹%.2f".formatted(payerName, totalAmount);
-                    String shareLine = "Each person pays: %.2f".formatted(perPersonShare);
-                    System.out.println(expenseLine);
-                    System.out.println(shareLine);
+                    if(friends.isEmpty())
+                    {
+                        System.out.println("no friends added yet");
+                    }
+                    else {
+                        System.out.print("Who paid? ");
+                        String payerName = input.nextLine();
+                        System.out.print("Total amount Rs ? ");
+                        double totalAmount = input.nextDouble();
+                        int numFriends = friends.size();
+                        double perPersonShare = totalAmount / numFriends;
+                        String expenseLine = "%s paid ₹%.2f".formatted(payerName, totalAmount);
+                        String shareLine = "Each person pays: %.2f".formatted(perPersonShare);
+                        System.out.println(expenseLine);
+                        System.out.println(shareLine);
+                    }
                 }
-                case 2 ->{
+                 case 2 ->{
+                     System.out.print("Friend name: ");
+                     String friendName = input.nextLine();
+                     friends.add(friendName);
+                     System.out.println("Added " + friendName + ".");
+                 }
+                 case 3 ->{
+                    if(friends.isEmpty())
+                    {
+                        System.out.println("no friends yet.");
+                    }
+                    else {
+                        for (String name : friends) {
+                            System.out.println("- " + name);
+                        }
+                    }
+                 }
+                case 0 ->{
                     System.out.println("Good bye....!!");
                     start=false;
                 }
